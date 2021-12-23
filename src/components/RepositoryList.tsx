f0a444a5fe693454fdf06b391f03ff1e3d7e9882
Gabriel Lima <grl@icomp.ufam.edu.br>
2021-12-22 21:58:33 -0400

    feat: adding typescript to the application

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -4,8 +4,14 @@ import { useState, useEffect } from 'react';
 const api = 'https://api.github.com/orgs/rocketseat/repos';
 import '../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repos, setRepos] = useState([]);
+    const [repos, setRepos] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch(api)
@@ -17,7 +23,7 @@ export function RepositoryList() {
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             <ul>
-            { repos.map(item => <RepositoryItem key={item.id} repository={item} />) }
+            { repos.map(item => <RepositoryItem key={item.name} repository={item} />) }
             </ul>
         </section>
     )
